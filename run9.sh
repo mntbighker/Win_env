f077399ee0e263d35334b73e9b7a8c91cdb48590
Mark Moorcroft <mntbighker@gmail.com>
2023-03-11 17:51:07 -0800

    Create run9.sh

---
+++ run9.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if ! [ -f $HOME/Win_env/.tmux.conf ]; then
+  echo -e "Please git clone into $HOME before running run.sh\n"
+  exit
+fi
+
+if ! [ -f /usr/bin/nvim ]; then
+  sudo dnf -y config-manager --set-enabled ol9_appstream
+  sudo dnf -y install oracle-epel-release-el9
+  sudo dnf -y group install "Development Tools"
+  sudo dnf -y install neovim python39 ruby ruby-devel rubygems luarocks zsh npm nodejs cargo cmake tmux wget
+fi
+
+cd $HOME
+rm -rf .config
+mv Win_env/.config .
+mv Win_env/.tmux.conf .
+mv Win_env/install.sh .
+gem install colorls
+cargo install tree-sitter-cli rust_fzf
+echo -e "### Type exit after the oh-my-zsh install script finishes, to complete setup ###\n"
+sh ./install.sh && rm ./install.sh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
+rm ./.zshrc
+mv Win_env/.zshrc .
+
+rm -rf Win_env
